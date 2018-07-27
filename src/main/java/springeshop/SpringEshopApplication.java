9fed91c6935b8c825c089d0fb3ebc9a188cbb769
koskarajohn <kvkarag@gmail.com>
2018-07-27 16:03:10 +0300

    first commit

---
+++ src/main/java/springeshop/SpringEshopApplication.java
@@ -0,0 +1,22 @@
+package springeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller	
+@SpringBootApplication
+public class SpringEshopApplication {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEshopApplication.class, args);
+	}
+}

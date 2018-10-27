e63de05a99a3fcdd624390cb771e843a6c3407b0
koskarajohn <kvkarag@gmail.com>
2018-10-27 09:13:51 +0300

    back to development

--- src/main/java/springeshop/controller/RoutesController.java
+++ src/main/java/springeshop/controller/RoutesController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestController {
+public class RoutesController {
 
 	@RequestMapping({ "/login", "/register", "/category", "/cart", "/product", "/checkout" })
 	   public String index() {

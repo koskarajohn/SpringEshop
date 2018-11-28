d225c8a0705466bc1dec73e1f11749979a5cab21
koskarajohn <kvkarag@gmail.com>
2018-11-28 14:27:43 +0200

    back to development

--- src/main/java/springeshop/configuration/RequestFilter.java
+++ src/main/java/springeshop/configuration/RequestFilter.java
@@ -47,10 +47,12 @@ public class RequestFilter extends OncePerRequestFilter{
 		Cookie cookie = new  Cookie("IS_AUTHENTICATED", isAuthenticated);
 		cookie.setMaxAge(3600000);
 		cookie.setPath("/");
-		//cookie.setDomain("localhost");
+		cookie.setDomain("localhost");
+		
 		//Heroku
-		cookie.setDomain("springeshop.herokuapp.com");
-		cookie.setSecure(true); 
+		//cookie.setDomain("springeshop.herokuapp.com");
+		//cookie.setSecure(true); 
+		
 		response.addCookie(cookie);
 	}
 	
@@ -58,10 +60,12 @@ public class RequestFilter extends OncePerRequestFilter{
 			cookie.setValue(null);
 			cookie.setMaxAge(0);
 			cookie.setPath("");
-			//cookie.setDomain("localhost");
+			cookie.setDomain("localhost");
+			
 			//Heroku
-			cookie.setDomain("springeshop.herokuapp.com");
-			cookie.setSecure(true);
+			//cookie.setDomain("springeshop.herokuapp.com");
+			//cookie.setSecure(true);
+			
 			response.addCookie(cookie);
 			createCookieIsAuthenticated(request, response);
 	}

2194ad798199451279c2a07d41689193ccb5f2ac
koskarajohn <kvkarag@gmail.com>
2018-11-28 13:41:19 +0200

    add breadcrumbs to product page

--- src/main/frontend/src/app/components/product-page/product-page.component.ts
+++ src/main/frontend/src/app/components/product-page/product-page.component.ts
@@ -7,6 +7,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 import { CartService } from 'src/app/services/cart.service';
 import { CartProduct } from 'src/app/models/cartProduct';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { NavigationCategory } from 'src/app/models/navigationCategory';
 
 @Component({
   selector: 'product-page',
@@ -26,6 +27,18 @@ export class ProductPageComponent implements OnInit, OnDestroy {
   isProductAvailable : boolean = false;
   isAddProductRequestDone : boolean = true;
 
+  productCategory : string ;
+  categoryRoute : string;
+  pageParam : number = 0;
+  categories = {
+    Vitamins : { greekName : 'Βιταμίνες', route : 'vitamins'},
+    Minerals : { greekName : 'Μέταλλα', route : 'minerals'},
+    Fishoils : { greekName : 'Ιχθυέλαια', route : 'fish-oils'},
+    Superfoods : { greekName : 'Υπερτροφές', route : 'superfoods'},
+    Fragrances : { greekName : 'Αρώματα', route : 'fragrances'},
+    Shampoos : { greekName : 'Σαμπουάν', route : 'shampoos'}
+  };
+
   @ViewChild(NavigationBarComponent) navigationBar : NavigationBarComponent;
   private isUserLoggedIn : boolean = false;
   private isLocalStorageEmpty : boolean = localStorage.length === 0;  
@@ -39,15 +52,12 @@ export class ProductPageComponent implements OnInit, OnDestroy {
     this.httpSubscription = this.productService.getSingleProduct(this.productNameParam)
                                               .subscribe(product => {
                                                 this.product = product;
+                                                this.productCategory = product.category.name === 'Fish-Oils' ? this.categories.Fishoils.greekName : this.categories[product.category.name].greekName;
+                                                this.categoryRoute = product.category.name === 'Fish-Oils' ? this.categories.Fishoils.route : this.categories[product.category.name].route;
                                                 this.isProductAvailable = product.quantity > 0;
                                                 this.productAvailability =  this.isProductAvailable ? this.available : this.notAvailable ;
                                               });
     
-  }
-
-  ngOnDestroy(){
-    this.routeSubscription.unsubscribe();
-    this.httpSubscription.unsubscribe();
   }
 
   increaseProductQuantity() : void{
@@ -101,6 +111,11 @@ export class ProductPageComponent implements OnInit, OnDestroy {
       this.cartService.addProductToAnonymousUserCart(cartProduct);
       this.navigationBar.setAnonymousUserCartCount();
     }
+  }
+
+  ngOnDestroy(){
+    this.routeSubscription.unsubscribe();
+    this.httpSubscription.unsubscribe();
   }
 
 }

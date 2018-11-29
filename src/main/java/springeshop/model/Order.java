9037d3b654e13708fa094a6d619d1faf0dd54103
koskarajohn <kvkarag@gmail.com>
2018-11-29 14:41:13 +0200

    database entities

---
+++ src/main/java/springeshop/model/Order.java
@@ -0,0 +1,135 @@
+package springeshop.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "order")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@Column(name = "email")
+	@Email
+	@NotNull(message = "Please provide email")
+	private String email;
+	
+	@Column(name = "status")
+	@NotNull(message = "Please provide status")
+	private String status;
+	
+	@NotNull
+	@Column(name ="order_date")
+	private Timestamp order_date;
+	
+	@Column(name ="payment_date")
+	private Timestamp payment_date;
+	
+	@Column(name ="shipping_date")
+	private Timestamp shipping_date;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "shipping_info_id")
+	private ShippingInfo shippingInfo;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "billing_info_id")
+	private BillingInfo billingInfo;
+	
+	public Order(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Timestamp getOrder_date() {
+		return order_date;
+	}
+
+	public void setOrder_date(Timestamp order_date) {
+		this.order_date = order_date;
+	}
+
+	public Timestamp getPayment_date() {
+		return payment_date;
+	}
+
+	public void setPayment_date(Timestamp payment_date) {
+		this.payment_date = payment_date;
+	}
+
+	public Timestamp getShipping_date() {
+		return shipping_date;
+	}
+
+	public void setShipping_date(Timestamp shipping_date) {
+		this.shipping_date = shipping_date;
+	}
+
+	public ShippingInfo getShippingInfo() {
+		return shippingInfo;
+	}
+
+	public void setShippingInfo(ShippingInfo shippingInfo) {
+		this.shippingInfo = shippingInfo;
+	}
+
+	public BillingInfo getBillingInfo() {
+		return billingInfo;
+	}
+
+	public void setBillingInfo(BillingInfo billingInfo) {
+		this.billingInfo = billingInfo;
+	}
+	
+	
+}
---
+++ src/main/java/springeshop/model/ShippingInfo.java
@@ -0,0 +1,135 @@
+package springeshop.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "shipping_info")
+public class ShippingInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	@NotNull(message = "Please provide first name")
+	@Size(min = 2, max = 200, message = "First name must be between 2 and 200 characters")
+	private String first_name;
+	
+	@Column(name = "last_name")
+	@NotNull(message = "Please provide last name")
+	@Size(min = 2, max = 200, message = "Last name must be between 2 and 200 characters")
+	private String last_name;
+	
+	@Column(name = "street_name")
+	@NotNull(message = "Please provide street name")
+	@Size(min = 2, max = 200, message = "Stret name must be between 2 and 200 characters")
+	private String street_name;
+	
+	@Column(name = "street_number")
+	@NotNull(message = "Please provide street number")
+	private int street_number;
+	
+	@Column(name = "post_code")
+	@NotNull(message = "Please provide post code")
+	private int post_code;
+	
+	@Column(name = "city")
+	@NotNull(message = "Please provide city")
+	@Size(min = 2, max = 200, message = "City must be between 2 and 200 characters")
+	private String city;
+	
+	@Column(name = "country")
+	@NotNull(message = "Please provide country")
+	@Size(min = 2, max = 200, message = "Country must be between 2 and 200 characters")
+	private String country;
+	
+	@Column(name = "phone")
+	@NotNull(message = "Please provide phone")
+	private String phone;
+	
+	public ShippingInfo(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getStreet_name() {
+		return street_name;
+	}
+
+	public void setStreet_name(String street_name) {
+		this.street_name = street_name;
+	}
+
+	public int getStreet_number() {
+		return street_number;
+	}
+
+	public void setStreet_number(int street_number) {
+		this.street_number = street_number;
+	}
+
+	public int getPost_code() {
+		return post_code;
+	}
+
+	public void setPost_code(int post_code) {
+		this.post_code = post_code;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}

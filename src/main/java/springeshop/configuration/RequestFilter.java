fecfb471159112350e3c94ca69b74716ac2665ed
koskarajohn <kvkarag@gmail.com>
2018-12-10 12:47:12 +0200

    heroku

--- src/main/java/springeshop/configuration/RequestFilter.java
+++ src/main/java/springeshop/configuration/RequestFilter.java
@@ -47,11 +47,11 @@ public class RequestFilter extends OncePerRequestFilter{
 		Cookie cookie = new  Cookie("IS_AUTHENTICATED", isAuthenticated);
 		cookie.setMaxAge(3600000);
 		cookie.setPath("/");
-		cookie.setDomain("localhost");
+		//cookie.setDomain("localhost");
 		
 		//Heroku
-		//cookie.setDomain("springeshop.herokuapp.com");
-		//cookie.setSecure(true); 
+		cookie.setDomain("springeshop.herokuapp.com");
+		cookie.setSecure(true); 
 		
 		response.addCookie(cookie);
 	}
@@ -60,11 +60,11 @@ public class RequestFilter extends OncePerRequestFilter{
 			cookie.setValue(null);
 			cookie.setMaxAge(0);
 			cookie.setPath("/");
-			cookie.setDomain("localhost");
+			//cookie.setDomain("localhost");
 			
 			//Heroku
-			//cookie.setDomain("springeshop.herokuapp.com");
-			//cookie.setSecure(true);
+			cookie.setDomain("springeshop.herokuapp.com");
+			cookie.setSecure(true);
 			
 			response.addCookie(cookie);
 			createCookieIsAuthenticated(request, response);
